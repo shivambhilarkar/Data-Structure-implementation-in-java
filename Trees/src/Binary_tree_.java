1392546c1ae28a63e97e72c13f73e77df162780d
Shivam Bhilarkar <shivambhilarkar@gmail.com>
2022-01-22 10:59:03 +0530

    .

---
+++ Trees/src/Binary_tree_.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Binary_tree_ {
+      public class Node{
+            int data;
+            Node left;
+            Node right;
+            Node(int val){
+                  this.data = val;
+                  left = null;
+                  right = null;
+            }
+      }
+
+      private static Node root;
+
+       Binary_tree_(){
+            root = null;
+      }
+
+
+      public static void main(String[] args){
+            Scanner scan = new Scanner (System.in);
+
+      }
+}
