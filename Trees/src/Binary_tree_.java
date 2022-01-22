3a1c7ddd1db8524bb7a0a9d46ec05c7a04df09fc
Shivam Bhilarkar <shivambhilarkar@gmail.com>
2022-01-22 19:35:22 +0530

    .

--- Trees/src/Binary_tree_.java
+++ Trees/src/Binary_tree_.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class Binary_tree_ {
-      public class Node{
+      public static class Node{
             int data;
             Node left;
             Node right;
-            Node(int val){
+            Node(int val, Node left, Node right){
                   this.data = val;
-                  left = null;
-                  right = null;
+                  this.left = left;
+                  this.right = right;
             }
       }
 
@@ -18,9 +18,38 @@ public class Binary_tree_ {
             root = null;
       }
 
+      public static  Node constructor(int[] arr, int high, int low ){
+            if(low  > high){
+                  return null;
+            }
+            int mid = (low + high) / 2;
+            int data = arr[mid];
+            Node left_child = constructor(arr, low, mid-1);
+            Node right_child = constructor(arr, mid+1, high);
+            Node node = new Node(data, left_child, right_child);
+            return node;
+      }
 
+      public static void display(Node node){
+            if(node == null){
+                  return;
+            }
+            String str = "";
+            str += node.left == null ? "." : node.left.data ;
+            str += " <- "+node.data +" -> ";
+            str += node.right == null ? "." : node.right.data;
+            System.out.println(str);
+
+            display(node.left);
+            display(node.right);
+      }
       public static void main(String[] args){
-            Scanner scan = new Scanner (System.in);
+            Scanner scan = new Scanner(System.in);
+            int[] arr = { 12,25,37,50,62,75,87};
+            int high = arr.length - 1;
+            int low = 0;
 
+            Node root = constructor(arr, low , high);                       //calling the constructor method to create the binary search tree..
+            display(root);
       }
 }
