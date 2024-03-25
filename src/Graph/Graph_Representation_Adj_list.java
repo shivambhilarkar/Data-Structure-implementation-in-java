f4ccf9c7c78d3207b1d7f551eafd8e78496df6ed
Shivam Bhilarkar <shivambhilarkar@gmail.com>
2024-03-25 13:14:00 +0530

    Graph representation with adjacency list

--- src/Graph/Graph_Representation_Adj_list.java
+++ src/Graph/Graph_Representation_Adj_list.java
@@ -3,67 +3,74 @@ package Graph;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+class Edge {
+    int src;
+    int dest;
+
+    Edge(int src, int dest) {
+        this.src = src;
+        this.dest = dest;
+    }
+}
+
+class Graph {
+
+
+    ArrayList<ArrayList<Edge>> adj;
+    int size;
+
+    Graph(int nodes) {
+        this.size = nodes;
+        adj = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            adj.add(new ArrayList<>());
+        }
+    }
+
+    public void add_directed_edge(int src, int dest) {
+        Edge edge = new Edge(src, dest);
+        adj.get(src).add(edge);
+    }
+
+    public void add_undirected_edge(int src, int dest) {
+        add_directed_edge(src, dest);
+        add_directed_edge(dest, src);
+    }
+
+    public void display_graph() {
+        int nodes = size;
+        for (int i = 0; i < nodes; i++) {
+            System.out.print("src : " + i + " : -> ");
+            for (Edge edge : adj.get(i)) {
+                System.out.print(edge.dest + " , ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+
 
 //for unweighted Graph
 public class Graph_Representation_Adj_list {
-      public static class Graph_{
-            public static class Edge{
-                  int src;
-                  int dest;
-                  Edge(int src, int dest){
-                        this.src = src;
-                        this.dest = dest;
-                  }
-            }
 
-            static ArrayList<ArrayList<Edge>> adj;
-            static int size;
-            Graph_(int nodes){
-                  this.size = nodes;
-                  adj = new ArrayList<> ();
-                  for(int i = 0; i < size; i++){
-                        adj.add(new ArrayList<> ());
-                  }
-            }
-
-            private static void add_directed_edge(int src, int dest){
-                  Edge edge = new Edge (src, dest);
-                  adj.get(src).add( edge );
-            }
-            private static void add_undirected_edge(int src, int dest){
-                  add_directed_edge (src, dest);
-                  add_directed_edge (dest, src);
-            }
-            private static void display_graph(){
-                  int nodes = size;
-                  for(int i = 0; i < nodes; i++){
-                        System.out.print("src : "+ i + " : -> ");
-                        for(Edge edge : adj.get(i)){
-                              System.out.print( edge.dest + " , ");
-                        }
-                        System.out.println();
-                  }
-                  System.out.println();
-            }
-      }
-
-      public static void main(String[] args){
-            Scanner scan = new Scanner (System.in);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
 
-            Graph_ graph = new Graph_ (6);
+        Graph graph = new Graph(6);
 
-            graph.add_undirected_edge (0,3);
-            graph.add_undirected_edge (0,1);
-            graph.add_undirected_edge (1,2);
-            graph.add_undirected_edge (2,3);
-            graph.add_undirected_edge (3,4);
-            graph.add_undirected_edge (4,5);
-            graph.add_undirected_edge (3,5);
+        graph.add_undirected_edge(0, 3);
+        graph.add_undirected_edge(0, 1);
+        graph.add_undirected_edge(1, 2);
+        graph.add_undirected_edge(2, 3);
+        graph.add_undirected_edge(3, 4);
+        graph.add_undirected_edge(4, 5);
+        graph.add_undirected_edge(3, 5);
 
-            graph.display_graph ();
+        graph.display_graph();
 
 
-      }
+    }
 
 }
---
+++ src/LinkedList/SinglyLinkedList.java
@@ -0,0 +1,145 @@
+package LinkedList;
+
+
+import java.util.Scanner;
+
+
+//Singly LinkedList Node
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+class LinkedList {
+
+    private Node head;
+
+    LinkedList() { }
+
+    public void add_at_head(int val) {
+        Node node = new Node(val);
+        if (head == null) {
+            head = node;
+        } else {
+            node.next = head;
+            head = node;
+        }
+    }
+
+    public void add_at_tail(int val) {
+        Node node = new Node(val);
+        if (head == null) {
+            head = node;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
+
+    public void delete_from_front() {
+        if (head == null) {
+            System.out.println("Underflow....");
+        } else {
+            int deleted = head.data;
+            head = head.next;
+            System.out.println("Deleted Front: " + deleted);
+        }
+    }
+
+    public void delete_from_last() {
+        if (head == null) {
+            System.out.println("Underflow....");
+        } else {
+            Node temp = head;
+            while (temp.next.next != null) {
+                temp = temp.next;
+            }
+            int deleted = temp.next.data;
+            temp.next = null;
+            System.out.println("Deleted Last : " + deleted);
+        }
+    }
+
+    public void remove_value_node(int val) {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("Underflow....");
+            return;
+        }
+        while (head.data == val) {
+            head = head.next;
+        }
+
+        while (temp.next != null) {
+            if (temp.next.data == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+    }
+
+    public void display_list() {
+        Node temp = head;
+        System.out.print("Linked List : ");
+        while (temp != null) {
+            System.out.print(temp.data + "  ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+
+}
+
+public class Linked_list_ {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        LinkedList list = new LinkedList();
+        //add head
+        list.add_at_head(10);
+        list.add_at_head(20);
+        list.add_at_head(30);
+        list.add_at_head(40);
+        list.display_list();
+
+        //add tail
+        list.add_at_tail(50);
+        list.add_at_tail(60);
+        list.display_list();
+
+        //delete head
+        list.delete_from_front();
+        list.display_list();
+
+        //delete last
+        list.delete_from_last();
+        list.display_list();
+
+        //remove specific node
+        list.remove_value_node(10);
+        list.display_list();
+
+        list.add_at_head(20);
+        list.add_at_head(20);
+        list.add_at_head(20);
+        list.add_at_head(30);
+        list.display_list();
+
+        list.remove_value_node(20);
+        list.display_list();
+    }
+}
+
+
